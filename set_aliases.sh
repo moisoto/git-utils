7dbdfb3aba78956ce932756eb0709e224adbdbf7
Moises Soto <35436843+moisoto@users.noreply.github.com>
2023-06-30 18:37:36 -0400

    Comment global config listing command.
    It's not really needed. 
    Decided to leave the command as a comment so users know how to list the global config if they want to check it.

--- set_aliases.sh
+++ set_aliases.sh
@@ -2,4 +2,6 @@
 # Notice the scripts are contained on ~/git-utils folder
 git config --global alias.df '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git-utils/git_df.sh'
 git config --global alias.tt '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git-utils/git_tt.sh'
-git config --global --list
+
+# Run the following command to check your current global git configuration:
+# git config --global --list
