433899ace915d7ef2c414d138c7c3d2c5a96c5bb
Moises Soto <35436843+moisoto@users.noreply.github.com>
2023-06-30 18:10:09 -0400

    Change path to git-utils folder

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,5 +1,5 @@
 # These commands need only be run once to configure the git aliases in your global config
-# Notice the scripts are contained on ~/git folder
-git config --global alias.df '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git/git_df.sh'
-git config --global alias.tt '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git/git_tt.sh'
+# Notice the scripts are contained on ~/git-utils folder
+git config --global alias.df '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git-utils/git_df.sh'
+git config --global alias.tt '! [ \"$GIT_PREFIX\" != \"\" ] && cd $GIT_PREFIX; ~/git-utils/git_tt.sh'
 git config --global --list
