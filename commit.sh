03cf79aa78aadcadeb0c1717ab80071361f4a4f0
Moises Soto <35436843+moisoto@users.noreply.github.com>
2025-06-28 09:16:44 -0400

    style: Fix grammar and status

--- commit.sh
+++ commit.sh
@@ -15,8 +15,8 @@
 # fi
 
 if ! command -v gum &> /dev/null; then
-printf "\033[38;2;255;85;85m✖ The gum utility is required to run this script.\033[0m\n" >&2
-printf "\033[38;2;255;85;85m  Please install it to continue.\033[0m\n" >&2
+  printf "\033[38;2;255;85;85m✖ The gum utility is required to run this script.\033[0m\n" >&2
+  printf "\033[38;2;255;85;85m  For install instructions please visit https://github.com/charmbracelet/gum.\033[0m\n" >&2
   exit 1
 fi
 
@@ -38,14 +38,14 @@ SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this chang
 DESCRIPTION=$(gum write --placeholder "Details of this change")
 
 if [ -z "$SUMMARY" ] ; then
-  echo "Can commit with an empty message. Aborting."
+  echo "Can't commit with an empty message. Aborting."
   exit
 fi
 
 echo "$SUMMARY"
 
 if [ -n "$DESCRIPTION" ] ; then
-  gum style --foreground 255 --border-foreground 255 --border normal "$DESCRIPTION"
+  gum style --foreground 255 --border-foreground 245 --border normal "$DESCRIPTION"
   # Commit these changes if user confirms
   gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
 else 
